baec543330cd2049d2738aeab56bd4fd3c751745
Elliott Coleman <ejaycoleman@googlemail.com>
2023-03-13 22:27:58 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { Avatar, Card, CardContent, Grid, Typography } from "@mui/material";
+import bg from "./bg.jpg";
+
+const App = () => {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{
+        minHeight: "100vh",
+        background: `url(${bg}) no-repeat center center fixed`,
+        backgroundSize: "cover",
+      }}
+    >
+      <Card style={{ padding: 30 }} elevation={10}>
+        <CardContent>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: 30,
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            <Avatar sx={{ width: 90, height: 90 }}>GC</Avatar>
+            <Typography variant="h3" style={{ marginTop: 20 }}>
+              Gabrielle Cummings
+            </Typography>
+            <Typography sx={{ mt: 1.5 }} color="text.secondary">
+              Fine Art / Curation
+            </Typography>
+          </div>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
+
+export default App;
